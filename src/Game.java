3a6e70b1edd9766603089ed08b9ba0a290aa78b0
khans4349 <khans4349@HRH-IC0027325.wrdsb.ca>
2017-01-20 10:02:55 -0500

    work in progress

--- src/Game.java
+++ src/Game.java
@@ -13,137 +13,126 @@ import javax.swing.JFrame;
  *
  * @author khans4349
  */
-
-
-public class Game extends JComponent implements KeyListener, MouseListener{
+public class Game extends JComponent implements KeyListener, MouseListener {
 
     // Height and Width of our game
     static final int WIDTH = 800;
     static final int HEIGHT = 600;
-    
     // sets the framerate and delay for our game
     // you just need to select an approproate framerate
     long desiredFPS = 60;
-    long desiredTime = (1000)/desiredFPS;
-    
+    long desiredTime = (1000) / desiredFPS;
     // game variables
-    
-    
     // create the size of each individual square to make the checkerboard
     int squares = 80;
-
     
+    boolean chip = false;
+
     // drawing of the game happens in here
     // we use the Graphics object, g, to perform the drawing
     // NOTE: This is already double buffered!(helps with framerate/speed)
     @Override
-    public void paintComponent(Graphics g)
-    {
+    public void paintComponent(Graphics g) {
         // always clear the screen first!
         g.clearRect(0, 0, WIDTH, HEIGHT);
-        
+
         // GAME DRAWING GOES HERE 
-        
+
         // Draw Background Screen
         g.setColor(Color.yellow);
         g.fillRect(0, 0, WIDTH, HEIGHT);
-        
+
         // set colour of the line of the squares
         g.setColor(Color.black);
-        
+
         // draw the squares
-        for(int x = 0; x < 6; x++){
-            for(int y = 0; y < 7; y++){
-                g.drawRect(120 + y*squares, 115 + x*squares, squares, squares);
+        for (int x = 0; x < 6; x++) {
+            for (int y = 0; y < 7; y++) {
+                g.drawRect(120 + y * squares, 115 + x * squares, squares, squares);
             }
         }
         // set the colour for the buttons
         g.setColor(Color.GRAY);
+       
         // draw the buttons
-        for(int x = 0; x < 1; x++){
-            for(int y = 0; y < 7; y++){
-                g.fillRect(120 + y*squares, 80 + x*squares, squares, 20);
-            }
+        for (int x = 0; x < 7; x++) {
+            g.fillRect(125 + (80*x), 80, 70, 20);
         }
-        
+
         // set colour of player ones chip
         g.setColor(Color.red);
-        
-        
-        
-//        g.fillOval(0, 0, squares, squares);
-        
+        if(chip == true){
+//            g.fillOval( , , 100, 100);
+        }
+
+
         // set colour of player twos chip
         g.setColor(Color.blue);
-       
-        
-        
-        
-        
+
+
+
+
+
         // GAME DRAWING ENDS HERE
     }
-    
-    
+
     // The main game loop
     // In here is where all the logic for my game will go
-    public void run()
-    {
+    public void run() {
         // Used to keep track of time used to draw and update the game
         // This is used to limit the framerate later on
         long startTime;
         long deltaTime;
-        
+
         // the main game loop section
         // game will end if you set done = false;
-        boolean done = false; 
-        while(!done)
-        {
+        boolean done = false;
+        while (!done) {
             // determines when we started so we can keep a framerate
             startTime = System.currentTimeMillis();
-            
+
             // all your game rules and move is done in here
             // GAME LOGIC STARTS HERE 
-            
-            
+
+
 
             // GAME LOGIC ENDS HERE 
-            
+
             // update the drawing (calls paintComponent)
             repaint();
-            
-            
-            
+
+
+
             // SLOWS DOWN THE GAME BASED ON THE FRAMERATE ABOVE
             // USING SOME SIMPLE MATH
             deltaTime = System.currentTimeMillis() - startTime;
-            try
-            {
-               if(deltaTime > desiredTime)
-               {
-                   //took too much time, don't wait
-                   Thread.sleep(1);
-               }else{
-                  // sleep to make up the extra time
-                 Thread.sleep(desiredTime - deltaTime);
-               }
-            }catch(Exception e){};
+            try {
+                if (deltaTime > desiredTime) {
+                    //took too much time, don't wait
+                    Thread.sleep(1);
+                } else {
+                    // sleep to make up the extra time
+                    Thread.sleep(desiredTime - deltaTime);
+                }
+            } catch (Exception e) {
+            };
         }
     }
-    
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // creates a windows to show my game
         JFrame frame = new JFrame("My Game");
-       
+
         // creates an instance of my game
         Game game = new Game();
         // sets the size of my game
-        game.setPreferredSize(new Dimension(WIDTH,HEIGHT));
+        game.setPreferredSize(new Dimension(WIDTH, HEIGHT));
         // adds the game to the window
         frame.add(game);
-         
+
         // sets some options and size of the window automatically
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -155,48 +144,46 @@ public class Game extends JComponent implements KeyListener, MouseListener{
         // starts my game loop
         game.run();
     }
-    
+
     @Override
     public void keyTyped(KeyEvent e) {
     }
-    
+
     @Override
-    public void keyPressed(KeyEvent e){
-        
+    public void keyPressed(KeyEvent e) {
     }
-    
+
     @Override
-    public void keyReleased(KeyEvent e){
-        
+    public void keyReleased(KeyEvent e) {
     }
 
     @Override
     public void mouseClicked(MouseEvent e) {
-        
     }
 
     @Override
     public void mousePressed(MouseEvent e) {
         int button = e.getButton();
+        if(button == MouseEvent.MOUSE_CLICKED){
+            chip = true;
+        }
+        
         int x = e.getX();
         int y = e.getY();
         System.out.println("clicked: (" + x + ", " + y + ")");
-        int circle = e.getButton();
         
+
     }
 
     @Override
     public void mouseReleased(MouseEvent e) {
-        
     }
 
     @Override
     public void mouseEntered(MouseEvent e) {
-        
     }
 
     @Override
     public void mouseExited(MouseEvent e) {
-        
     }
 }
